a6c4c831ecaaf10d9676f7adbc055ed128d478db
Arya <aryaajitnair@gmail.com>
2022-02-11 18:58:33 +0530

    Add Name and social ID

--- src/components/LeftBar.js
+++ src/components/LeftBar.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
+
+
 export default function LeftBar() {
   return (
-    <div className="left-bar">LeftBar</div>
+    <div className="left-bar">
+      <img src="https://picsum.photos/200" alt="Arya-img" className="arya-image"/>
+      <div className="Name">Arya Nair</div>
+      <a href="https://github.com/Arya-A-Nair" className="github-handle">@Arya-A-Nair</a>
+
+      <div className="role">Python, C, C++, Web Development</div>
+      <span><i className="fas fa-map-marker-alt"></i> Earth</span>
+      <div className="socials">
+        <div className="social-icon">
+          <a href="https://www.linkedin.com/in/arya-nair-2003/" target="_blank" class="socials"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+        <div className="social-icon">
+          <a href="mailto: aryaajitnair@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+        </div>
+        <div className="social-icon">
+          <a href="https://github.com/Arya-A-Nair"><i class="fa-brands fa-github"></i></a>
+        </div>
+      </div>
+    
+      
+    </div>
   )
 }
