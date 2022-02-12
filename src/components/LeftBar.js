258afb22433d2c64df35244cf365a0ed1c600bdd
Arya <aryaajitnair@gmail.com>
2022-02-12 12:13:50 +0530

    Left Bar complete

--- src/components/LeftBar.js
+++ src/components/LeftBar.js
@@ -1,27 +1,20 @@
 import React from 'react'
-
+import logo from "./assets/logo.png"
+import Socials from "./Socials.js"
 
 
 export default function LeftBar() {
   return (
     <div className="left-bar">
-      <img src="https://picsum.photos/200" alt="Arya-img" className="arya-image"/>
+    
+      <img src={logo} alt="Arya-img" className="arya-image"/>
       <div className="Name">Arya Nair</div>
-      <a href="https://github.com/Arya-A-Nair" className="github-handle">@Arya-A-Nair</a>
+      <a href="https://github.com/Arya-A-Nair" className="github-handle" target="_blank">@Arya-A-Nair</a>
 
-      <div className="role">Python, C, C++, Web Development</div>
+      <div className="role">Responsive Web Development, Python, C++</div>
       <span><i className="fas fa-map-marker-alt"></i> Earth</span>
-      <div className="socials">
-        <div className="social-icon">
-          <a href="https://www.linkedin.com/in/arya-nair-2003/" target="_blank" class="socials"><i class="fab fa-linkedin-in"></i></a>
-        </div>
-        <div className="social-icon">
-          <a href="mailto: aryaajitnair@gmail.com"><i class="fa-solid fa-envelope"></i></a>
-        </div>
-        <div className="social-icon">
-          <a href="https://github.com/Arya-A-Nair"><i class="fa-brands fa-github"></i></a>
-        </div>
-      </div>
+      <Socials/>
+      
     
       
     </div>
---
+++ src/components/Socials.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Socials() {
+  return (
+    <div className="socials">
+        <div className="social-icon">
+          <a href="https://www.linkedin.com/in/arya-nair-2003/" target="_blank" class="socials"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+        <div className="social-icon">
+          <a href="mailto: aryaajitnair@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+        </div>
+        <div className="social-icon">
+          <a href="https://github.com/Arya-A-Nair"><i class="fa-brands fa-github"></i></a>
+        </div>
+    </div>
+  )
+}
