45937b5de2fcaaedff83900611006b45b259a035
Arya Nair <aryaajitnair@gmail.com>
2022-09-01 09:47:19 +0530

    Update data.js

--- src/components/assets/data.js
+++ src/components/assets/data.js
@@ -3,14 +3,17 @@ const resume=[
 ]
 
 const certifications=[
+    {text:"Certificate by Udemy", link:"https://www.udemy.com/certificate/UC-3d786140-e231-4d89-bc89-33ff06aba723/",title:"Full Stack Web Development With React, MUI, Django, And DRF"},
     {text:"Certificate by IBM",link:"https://www.credly.com/badges/3ce53246-2cd2-4245-a5ce-9e95e731b637?source=linked_in_profile",title:"Python For Data Science"},
     {text:"Certificate by freecodecamp",link:"https://www.freecodecamp.org/certification/arya-nair/scientific-computing-with-python-v7",title:"Scientific Computing with Python"},
     {text:"Certificate by freecodecamp",link:"https://www.freecodecamp.org/certification/arya-nair/responsive-web-design",title:"Responsive Web Design"},
-    {text:"Certificate by freecodecamp",link:"https://www.freecodecamp.org/certification/arya-nair/javascript-algorithms-and-data-structures",title:"Javascript DSA"},
+    {text:"Certificate by freecodecamp",link:"https://www.freecodecamp.org/certification/arya-nair/javascript-algorithms-and-data-structures",title:"Javascript DSA"}
 ]
 
 const works=[
-    {text:"Mailing system using Django+React using Django Rest framwork",link:"https://github.com/Arya-A-Nair/MAIL-PROJECT",title:"Mail Project"},
+    {text:"Team management Application Django+React",link:"https://github.com/Arya-A-Nair/Team-Manager",title:"Team Manager"},
+    {text:"Task management Application Django+React",link:"https://github.com/Arya-A-Nair/Task-Manager",title:"Task Manager"},
+    {text:"Mailing system using Django+React using Django Rest framework",link:"https://github.com/Arya-A-Nair/MAIL-PROJECT",title:"Mail Project"},
     {text:"This project links to my old personal website",link:"https://arya-a-nair.github.io/Personal-Website/",title:"Old Personal Website"},
     {text:"Python project to Detect movement",link:"https://github.com/Arya-A-Nair/Face-Detection",title:"Face Detection"},
     {text:"Simple Bootstrap website made for a friend",link:"https://github.com/Arya-A-Nair/Pratherium",title:"Pratherium"},
